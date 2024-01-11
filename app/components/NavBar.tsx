a969cb6c1807c5575ef3a11b95d8c7ef86a53e97
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2024-01-11 14:17:04 +0100

    Fixed comments on PR

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,26 +4,48 @@ import { OpenEPILogo } from "@/app/icons/OpenEPILogo";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import Box from "@mui/material/Box";
-import {ExternalLinkIcon} from "@/app/icons/ExternalLinkIcon";
-import {usePathname} from "next/navigation";
-import {useEffect} from "react";
+import { ExternalLinkIcon } from "@/app/icons/ExternalLinkIcon";
+import { usePathname } from "next/navigation";
+import { palettes } from "@/app/theme/palettes";
 
-export default function NavBar () {
+export default function NavBar() {
   const currentRoute = usePathname();
+  const baseStyle: string = "px-6 py-2 rounded-full";
+
+  const linkClassName = (path: string) =>
+    currentRoute === path
+      ? baseStyle + ` bg-[${palettes.secondary["90"]}]`
+      : baseStyle + " hover:bg-[#1d1b2014]";
 
   return (
-    <nav className="flex flex-row  items-center m-[48px] justify-between">
-      <Box className="flex flex-row gap-[19px]" >
-        <OpenEPILogo/>
-        <Typography variant={'h1'}  className="text-[38px]">| Developer Portal</Typography>
+    <nav className="flex flex-row items-center m-12 justify-between">
+      <Box className="flex flex-row gap-5 items-center">
+        <OpenEPILogo />
+        <Typography className="text-4xl">| Developer Portal</Typography>
       </Box>
-      <Box className="flex flex-row text-[22px]">
-        <Link href="/data-catalog" className={currentRoute === '/data-catalog/' ? "px-[24px] py-[8px] rounded-[40px] bg-[#D1E8D5]" : "px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]"}>Data catalog</Link>
-        <Link href="/credentials" className={currentRoute === '/credentials/' ? "px-[24px] py-[8px] rounded-[40px] bg-[#D1E8D5]" : "px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]"}>Credentials</Link>
-        <Link href="/resources" className={currentRoute === '/resources/' ? "px-[24px] py-[8px] rounded-[40px] bg-[#D1E8D5]" : "px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]"}>Resources</Link>
-        <Link href="/contact" className={currentRoute === '/contact/' ? "px-[24px] py-[8px] rounded-[40px] bg-[#D1E8D5]" : "px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]"}>Contact</Link>
-        <Link href="/" className="flex flex-row items-center gap-[6px] px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">About the project <ExternalLinkIcon/></Link>
+      <Box className="flex flex-row text-2xl">
+        <Link href="/data-catalog" className={linkClassName("/data-catalog/")}>
+          Data catalog
+        </Link>
+        <Link href="/credentials" className={linkClassName("/credentials/")}>
+          Credentials
+        </Link>
+        <Link href="/resources" className={linkClassName("/resources/")}>
+          Resources
+        </Link>
+        <Link href="/contact" className={linkClassName("/contact/")}>
+          Contact
+        </Link>
+        <Link
+          href="/"
+          className={
+            baseStyle +
+            " flex flex-row items-center gap-1.5 hover:bg-[#1d1b2014]"
+          }
+        >
+          About the project <ExternalLinkIcon />
+        </Link>
       </Box>
     </nav>
-  )
+  );
 }
