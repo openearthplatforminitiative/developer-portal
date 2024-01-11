e07fbab809e99e1dd9cc40f914549f089844ef95
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2024-01-11 11:48:21 +0100

    Added hover to nav bar items

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { OpenEPILogo } from "@/app/icons/OpenEPILogo";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import Box from "@mui/material/Box";
+import {ExternalLinkIcon} from "@/app/icons/ExternalLinkIcon";
 
 export default function NavBar () {
   return (
@@ -11,11 +12,11 @@ export default function NavBar () {
         <Typography variant={'h1'}  className="text-[38px]">| Developer Portal</Typography>
       </Box>
       <Box className="flex flex-row text-[22px] gap-[8px]">
-        <Link href="/data-catalog" className=" px-[24px] py-[8px]">Data catalog</Link>
-        <Link href="/credentials" className=" px-[24px] py-[8px]">Credentials</Link>
-        <Link href="/resources" className=" px-[24px] py-[8px]">Resources</Link>
-        <Link href="/contact" className=" px-[24px] py-[8px]">Contact</Link>
-        <Link href="/" className=" px-[24px] py-[8px]">About the project</Link>
+        <Link href="/data-catalog" className="px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">Data catalog</Link>
+        <Link href="/credentials" className="px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">Credentials</Link>
+        <Link href="/resources" className="px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">Resources</Link>
+        <Link href="/contact" className="px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">Contact</Link>
+        <Link href="/" className="flex flex-row items-center gap-[6px] px-[24px] py-[8px] rounded-[40px] hover:bg-[#1D1B20] hover:bg-opacity-[0.08]">About the project <ExternalLinkIcon/></Link>
       </Box>
     </nav>
   )
