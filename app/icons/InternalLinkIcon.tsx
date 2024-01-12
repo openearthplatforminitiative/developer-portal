270957991cfae2de0bd2a81b15018a81c67dc0ee
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2024-01-12 12:28:01 +0100

    First contents of resources page added

---
+++ app/icons/InternalLinkIcon.tsx
@@ -0,0 +1,16 @@
+export const InternalLinkIcon = () => {
+  return (
+    <svg
+      width='22'
+      height='22'
+      viewBox='0 0 22 22'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M11.0003 3.66666L9.70783 4.95916L14.8228 10.0833H3.66699V11.9167H14.8228L9.70783 17.0408L11.0003 18.3333L18.3337 11L11.0003 3.66666Z'
+        fill='black'
+      />
+    </svg>
+  );
+};
--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,10 +1,182 @@
-import Link from 'next/link';
+'use client';
 
-export default function Home() {
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Card, CardActions, CardContent } from '@mui/material';
+import { ExternalLinkIcon } from '@/app/icons/ExternalLinkIcon';
+import { InternalLinkIcon } from '@/app/icons/InternalLinkIcon';
+
+const Home = () => {
   return (
-    <main>
-      <h1>Resources</h1>
-      <Link href='/'>Go home</Link>
+    <main className={'ml-[300px] mr-[387px] my-44'}>
+      <Box className={'flex flex-col gap-8 mb-20'}>
+        <Typography className={'text-5xl'}>Open source resources</Typography>
+        <Typography className={'text-2xl'}>
+          Design and frontend resources, policies and specifications.
+        </Typography>
+      </Box>
+      <Box className={'flex flex-col gap-32'}>
+        <Box className={'flex flex-col gap-8'}>
+          <Typography className={'text-4xl'}>Documents</Typography>
+          <Box className={'grid grid-cols-3 gap-6'}>
+            <Card className={'bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Our open policy
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://mui.com'}>
+                    <InternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+            <Card className={'bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Our metadata policy
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://mui.com'}>
+                    <InternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+            <Card className={'bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    OpenAPI specification
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://mui.com'}>
+                    <ExternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
+
+        <Box className={'flex flex-col gap-8'}>
+          <Typography className={'text-4xl'}>Frontend</Typography>
+          <Box className={'grid grid-cols-3 gap-6'}>
+            <Card className={'w-fit bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <img src={'/mui.png'} alt={'mui logo'} />
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Material UI
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://mui.com'}>
+                    <ExternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
+        <Box className={'flex flex-col gap-8'}>
+          <Typography className={'text-4xl'}>Design</Typography>
+          <Box className={'grid auto-cols-max gap-6'}>
+            <Card className={'w-fit bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <img src={'/google-fonts.png'} alt={'mui logo'} />
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Google fonts
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://fonts.google.com/'}>
+                    <ExternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+            <Card className={'w-fit bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <img src={'/material-design.png'} alt={'mui logo'} />
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Material Design
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a target={'_blank'} href={'https://m3.material.io/'}>
+                    <ExternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+            <Card className={'w-fit bg-[#F2F4EF] hover:bg-secondary-90'}>
+              <img src={'/material-symbols.png'} alt={'mui logo'} />
+              <CardContent
+                className={'flex flex-row items-center justify-between'}
+              >
+                <Box className={'flex flex-col'}>
+                  <Typography variant='h5' component='div'>
+                    Material Symbols
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Sub text
+                  </Typography>
+                </Box>
+                <CardActions>
+                  <a
+                    target={'_blank'}
+                    href={
+                      'https://fonts.google.com/icons?selected=Material+Symbols+Outlined:arrow_back:FILL@0;wght@400;GRAD@0;opsz@24'
+                    }
+                  >
+                    <ExternalLinkIcon />
+                  </a>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
+      </Box>
     </main>
   );
-}
+};
+
+export default Home;
