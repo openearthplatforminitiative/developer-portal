9088407176e67c4b4cf42490324881ae0a1ae81a
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2024-01-17 15:54:54 +0100

    Most of fab is in place. Some styling is still needed

---
+++ app/icons/MessageIcon.tsx
@@ -0,0 +1,14 @@
+export const MessageIcon = () => (
+  <svg
+    width='24'
+    height='25'
+    viewBox='0 0 24 25'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M20 2.32996H4C2.9 2.32996 2 3.22996 2 4.32996V22.33L6 18.33H20C21.1 18.33 22 17.43 22 16.33V4.32996C22 3.22996 21.1 2.32996 20 2.32996ZM20 16.33H6L4 18.33V4.32996H20V16.33Z'
+      fill='#006D41'
+    />
+  </svg>
+);
