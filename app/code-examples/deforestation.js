c74d35bb4b8e327d239b4df9a4656c9b1ae401da
Gil Tinde <gil.tinde@pm.me>
2024-01-25 14:18:46 +0100

    feat: add deforestation description and examples

--- app/code-examples/deforestation.js
+++ app/code-examples/deforestation.js
@@ -1,11 +1,15 @@
-async function getProfile(accessToken) {
-  let accessToken = localStorage.getItem('access_token');
+const response = await fetch(
+  'https://api-test.openepi.io/deforestation/basin?' +
+    new URLSearchParams({
+      lon: '30.0619',
+      lat: '-1.9441',
+      start_year: '2010',
+      end_year: '2019',
+    })
+);
+const data = await response.json();
 
-  const response = await fetch('https://developer.openepi.io/v1/me', {
-    headers: {
-      Authorization: 'Bearer ' + accessToken,
-    },
-  });
+// Get the total forest cover loss within the returned river basin over the time period
+const loss = json.features[0].properties.daterange_tot_treeloss;
 
-  const data = await response.json();
-}
+console.log(`Total forest cover loss: ${loss} km^2`);
