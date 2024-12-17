13636f4059e70a24c10fc7bb2003fd37c7a77b59
Sander Krøglid <sander.kroglid@knowit.no>
2024-12-17 12:45:16 +0100

    fix: fix env variable

--- app/actions.ts
+++ app/actions.ts
@@ -7,6 +7,6 @@ export async function signOut() {
 	const cookieStore = await cookies()
 	cookieStore.delete("username")
 	redirect(
-		`/dashboard/oauth2/sign_out?rd=https://${process.env.auth_domain}/realms/openepi/protocol/openid-connect/logout?client_id=openepi-api-gateway`
+		`/dashboard/oauth2/sign_out?rd=https://${process.env.AUTH_DOMAIN}/realms/openepi/protocol/openid-connect/logout?client_id=openepi-api-gateway`
 	)
 }
