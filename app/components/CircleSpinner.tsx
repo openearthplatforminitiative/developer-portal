ff440eef119c17e0705727df710646e26f32559a
Sander Krøglid <sander.kroglid@knowit.no>
2025-01-10 15:53:34 +0100

    feat: adds draft for informing about registering on landing apge

--- app/components/CircleSpinner.tsx
+++ app/components/CircleSpinner.tsx
@@ -1,22 +1,38 @@
+import { Box } from "@mui/material"
+
 interface SpinnerProps {
 	height: number
 	width: number
 }
-export const CircleSpinner = ({ height, width }: SpinnerProps) => (
+
+const Circle = () => (
 	<svg
-		width={width}
-		height={height}
-		viewBox="0 0 639 639"
+		className="h-full animate-spinFrom90"
+		viewBox="0 0 600 600"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<circle
-			cx="319.5"
-			cy="319.5"
-			r="313.5"
+			cx="300"
+			cy="300"
+			r="253"
+			stroke="#77DAA0"
+			strokeWidth="6"
+			strokeDasharray="24 24 48 24 64 24"
+		/>
+		<circle
+			cx="300"
+			cy="300"
+			r="293"
 			stroke="#77DAA0"
 			strokeWidth="6"
 			strokeDasharray="24 24 48 24 64 24"
 		/>
 	</svg>
+)
+
+export const CircleSpinner = () => (
+	<Box className="absolute h-full aspect-square -left-[20%] -bottom-[20%]">
+		<Circle />
+	</Box>
 )
