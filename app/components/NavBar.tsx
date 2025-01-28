937198d825d20cb3b285d1a554df707a12ae2d50
Sander Krøglid <sander.kroglid@knowit.no>
2025-01-28 13:10:01 +0100

    feat: add user information to menu and more responsive navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 import Box from "@mui/material/Box"
 import { usePathname } from "next/navigation"
 import { Button, IconButton, Menu, MenuItem } from "@mui/material"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import {
 	ArrowOutward,
 	GridView,
@@ -29,7 +29,7 @@ const NavBar = () => {
 	const currentRoute = usePathname()
 
 	const baseStyle: string =
-		"px-6 py-2 rounded-full flex flex-row items-center gap-1.5 hover:bg-[#1d1b2014]"
+		"px-6 py-2 rounded-full flex flex-row items-center gap-1.5 hover:bg-neutral-95"
 
 	const linkClassName = (path: string, primary?: boolean) => {
 		return currentRoute.startsWith(path)
@@ -69,24 +69,41 @@ const NavBar = () => {
 		signIn("keycloak", { callbackUrl: "/dashboard" })
 	}
 
+	const isCurrentRoute = (route: string) => currentRoute.startsWith(route)
+
 	const handleSignOut = () => {
-		if (currentRoute.startsWith("/dashboard")) {
+		if (isCurrentRoute("/dashboard")) {
 			signOut({ redirect: true, callbackUrl: "/" })
 		} else signOut({ redirect: false })
 	}
 
+	const compressUsername = (username: string) => {
+		const newUsername = username.split("@")[0]
+		if (newUsername.length > 14) {
+			return newUsername.slice(0, 10) + "..."
+		}
+		return newUsername
+	}
+	const getInitials = (name = "") =>
+		name
+			.split(" ")
+			.map((part) => part[0])
+			.slice(0, 2)
+			.join("")
+			.toUpperCase()
+
 	return (
-		<nav className="flex flex-row sticky top-0 z-20 bg-[#FBFDF8] backdrop-blur-sm w-full items-center sm:px-12 px-6 sm:py-4 py-2 sm:my-8 my-4 justify-between">
+		<nav className="flex flex-row sticky top-0 z-20 bg-[#FBFDF8] backdrop-blur-sm w-full items-center sm:px-6 px-4 sm:py-4 py-2 sm:my-8 my-4 justify-between">
 			<Link href="/">
-				<Box className="flex lg:flex-row flex-col gap-5 items-center">
-					<Box className="xs:flex hidden">
+				<Box className="flex gap-5 items-center">
+					<Box className="2xl:inline hidden">
 						<OpenEPILogoLarge />
 					</Box>
-					<Box className="xs:hidden flex">
+					<Box className="2xl:hidden inline">
 						<OpenEPILogo />
 					</Box>
-					<Typography className="lg:text-4xl 2xl:inline hidden">
-						| Developer Portal
+					<Typography className="xs:text-4xl text-2xl">
+						<span className="2xl:inline hidden">|</span> Developer Portal
 					</Typography>
 				</Box>
 			</Link>
@@ -103,30 +120,40 @@ const NavBar = () => {
 				<Link
 					href="https://openepi.io"
 					target="_blank"
-					className={baseStyle + " hover:bg-[#1d1b2014]"}
+					className={baseStyle + " hover:bg-neutral-95"}
 				>
 					About the project <ArrowOutward />
 				</Link>
 				{user ? (
 					<>
-						<Button
+						<button
 							onClick={handleDropdown}
-							className={linkClassName("/dashboard", true)}
+							className="flex justify-center text-xl size-12 text-primary-100 bg-primary-40 rounded-full items-center gap-1 mr-1 hover:bg-neutral-90 hover:text-neutral-0"
 						>
-							<Person />
-							{user}
-							{isDropdownOpen ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-						</Button>
+							{getInitials(user)}
+						</button>
 						<Menu
 							open={isDropdownOpen}
 							anchorEl={anchorElDropdown}
 							onClose={handleDropdownClose}
 							anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
 							transformOrigin={{ vertical: "top", horizontal: "right" }}
-							className="shadow-none"
+							className="shadow-none mt-2"
 						>
+							<Box className="absolute pt-6 bg-neutral-95 w-full h-16 top-0 left-0" />
+							<Box className="px-4 py-6 min-w-64 items-center break-words flex flex-col gap-4">
+								<Box className="flex justify-center text-xl size-14 z-10 text-primary-100 bg-primary-40 rounded-full items-center">
+									{getInitials(user)}
+								</Box>
+								<Box className="break-words">
+									<Typography className="text-lg">{user}</Typography>
+								</Box>
+							</Box>
 							<Link href="/dashboard">
-								<MenuItem onClick={handleDropdownClose} className="p-4 w-44">
+								<MenuItem
+									onClick={handleDropdownClose}
+									className={`p-4 ${isCurrentRoute("/dashboard") && "bg-secondary-90"}`}
+								>
 									<Typography className="text-xl flex items-center gap-1">
 										<GridView /> Dashboard
 									</Typography>
@@ -144,7 +171,7 @@ const NavBar = () => {
 						onClick={handleSignIn}
 						className={
 							baseStyle +
-							" bg-primary-40 text-neutral-100 hover:bg-[#1d1b2014] hover:text-neutral-0"
+							" normal-case bg-primary-40 text-neutral-100 hover:bg-neutral-95 hover:text-neutral-0"
 						}
 					>
 						Sign in <Login />
@@ -166,23 +193,46 @@ const NavBar = () => {
 					disableScrollLock={true}
 					anchorEl={anchorEl}
 				>
+					{user && (
+						<Box className="px-4 py-6 min-w-64 items-center break-words flex flex-col gap-4">
+							<Box className="absolute pt-6 bg-neutral-95 w-full h-16 top-0 left-0" />
+							<Box className="flex justify-center text-xl size-14 z-10 text-primary-100 bg-primary-40 rounded-full items-center">
+								{getInitials(user)}
+							</Box>
+							<Box className="break-words">
+								<Typography className="text-lg">{user}</Typography>
+							</Box>
+						</Box>
+					)}
 					<Link href="/data-catalog">
-						<MenuItem onClick={handleClose} className="py-3 px-6 flex gap-2">
+						<MenuItem
+							onClick={handleClose}
+							className={`p-4 flex gap-2 ${isCurrentRoute("/data-catalog") && "bg-secondary-90"}`}
+						>
 							<Typography className="text-lg">Data catalog</Typography>
 						</MenuItem>
 					</Link>
 					<Link href="/resources">
-						<MenuItem onClick={handleClose} className="py-3 px-6 flex gap-2">
+						<MenuItem
+							onClick={handleClose}
+							className={`p-4 flex gap-2 ${isCurrentRoute("/resources") && "bg-secondary-90"}`}
+						>
 							<Typography className="text-lg">Resources </Typography>
 						</MenuItem>
 					</Link>
-					<MenuItem onClick={handleClose} className="py-3 px-6 flex gap-2">
-						<Link href="/contact">
+					<Link href="/contact">
+						<MenuItem
+							onClick={handleClose}
+							className={`p-4 flex gap-2 ${isCurrentRoute("/contact") && "bg-secondary-90"}`}
+						>
 							<Typography className="text-lg">Contact</Typography>
-						</Link>
-					</MenuItem>
+						</MenuItem>
+					</Link>
 					<Link href="https://openepi.io" target="_blank">
-						<MenuItem onClick={handleClose} className="py-3 px-6 flex gap-2">
+						<MenuItem
+							onClick={handleClose}
+							className={`p-4 flex gap-2 ${isCurrentRoute("/about") && "bg-secondary-90"}`}
+						>
 							<Typography className="text-lg">About the project</Typography>
 							<ArrowOutward />
 						</MenuItem>
@@ -192,16 +242,13 @@ const NavBar = () => {
 							<Link href="/dashboard">
 								<MenuItem
 									onClick={handleClose}
-									className="py-3 px-6 flex gap-2"
+									className={`p-4 flex gap-2 ${isCurrentRoute("/dashboard") && "bg-secondary-90"}`}
 								>
 									<Typography className="text-lg">Dashboard</Typography>
-									<Person />
+									<GridView />
 								</MenuItem>
 							</Link>
-							<MenuItem
-								onClick={handleSignOut}
-								className="py-3 px-6 flex gap-2"
-							>
+							<MenuItem onClick={handleSignOut} className="p-4 flex gap-2">
 								<Typography className="text-lg">Sign out</Typography>
 								<Logout />
 							</MenuItem>
