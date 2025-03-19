608495807af55e53b6897d3058728b7d7aea5565
Sander Krøglid <sander.kroglid@knowit.no>
2025-03-19 09:08:22 +0100

    fix: switching cursor on map modes

--- app/components/DataCatalog/DrawControl/DrawControl.tsx
+++ app/components/DataCatalog/DrawControl/DrawControl.tsx
@@ -8,6 +8,7 @@ import { DrawPolygonControl } from "./Polygon/DrawPolygonControl";
 import { DrawPolygonProvider } from "./Polygon/DrawPolygonProvider";
 import { DrawSelectControl } from "./Select/DrawSelectControl";
 import { useMap } from "react-map-gl/maplibre";
+import { useEffect } from "react";
 
 export const DrawControl = () => {
 
@@ -19,6 +20,22 @@ export const DrawControl = () => {
     return 'hover:bg-neutral-90 border border-[#e5e7eb] border-r-0 p-2 first:rounded-l-lg last:rounded-r-lg last:border-r ' + (active ? 'bg-neutral-90' : 'bg-neutral-95');
   }
 
+  useEffect(() => {
+    if (map.current) {
+      switch (tool) {
+        case 'polygon':
+          map.current.getCanvas().style.cursor = 'crosshair';
+          break;
+        case 'point':
+          map.current.getCanvas().style.cursor = 'crosshair';
+          break;
+        case 'select':
+          map.current.getCanvas().style.cursor = '';
+          break;
+      }
+    }
+  }, [tool, map])
+
   DrawPoint()
 
   const handleToolChange = (tool: DrawTool) => {
@@ -26,25 +43,15 @@ export const DrawControl = () => {
     switch (tool) {
       case 'polygon':
         setMode('drawing')
-        if (map.current) {
-          map.current.getCanvas().style.cursor = 'crosshair';
-        }
         break;
       case 'point':
         setMode('drawing')
-        if (map.current) {
-          map.current.getCanvas().style.cursor = 'crosshair';
-        }
         break;
       case 'select':
         setMode('selecting')
-        if (map.current) {
-          map.current.getCanvas().style.cursor = '';
-        }
         break;
     }
   }
-
 
   return (
     <>
@@ -53,8 +60,8 @@ export const DrawControl = () => {
       </DrawPolygonProvider>
       <DrawSelectControl />
       <DrawPointSource />
-      <div className="absolute bottom-0 p-4 w-full flex justify-center gap-2">
-        <div className="w-min flex flex-row shadow-md rounded-lg">
+      <div className="absolute bottom-0 p-4 w-full flex justify-center gap-2 pointer-events-none">
+        <div className="w-min flex flex-row shadow-md rounded-lg pointer-events-auto">
           <button className={buttonStyle(tool == 'select')} onClick={() => handleToolChange('select')}><PanToolOutlined /></button>
           <button className={buttonStyle(tool == 'polygon')} onClick={() => handleToolChange('polygon')}><Architecture /></button>
           <button className={buttonStyle(tool == 'point')} onClick={() => handleToolChange('point')}><PlaceOutlined /></button>
