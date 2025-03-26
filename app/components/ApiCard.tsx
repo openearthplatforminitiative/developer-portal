b0d684dc421766b93df3862db8187014754f2d4f
Sander Krøglid <sander.kroglid@knowit.no>
2025-03-26 15:31:39 +0100

    fix: small layout changes

--- app/components/ApiCard.tsx
+++ app/components/ApiCard.tsx
@@ -17,10 +17,9 @@ const ApiCard = ({ header, subHeader, cardIcon, href }: CardProps) => {
 	return (
 		<Link
 			href={href}
-			className="flex flex-1 md:min-w-[280px] min-w-[230px] 2xs:max-w-[calc(100%-0.75rem)] sm:max-w-[calc(48%-0.75rem)] lg:max-w-[calc(31%-0.75rem)]"
 		>
-			<Card className="group w-full bg-card hover:bg-secondary-90 relative overflow-visible transition duration-300 shadow-none rounded-xl">
-				<Box className="absolute -left-6 -top-6">{cardIcon}</Box>
+			<Card className="group w-full h-full bg-card hover:bg-secondary-90 relative overflow-visible transition duration-300 shadow-none rounded-xl">
+				<Box className="absolute -left-6 -top-6 size-12 flex items-center justify-center bg-primary-40 rounded-full text-neutral-100">{cardIcon}</Box>
 				<Box className="flex flex-col pt-8 pb-10 pr-6 pl-6">
 					<Box className="flex flex-row justify-between my-2">
 						<Typography className="text-2xl">{header}</Typography>
