0d1025f83838d74a9b74f495fc3d458a3998fa47
Sander Krøglid <sander.kroglid@knowit.no>
2025-03-26 15:30:47 +0100

    fix: add category type

--- app/data-catalog/DataCatalogTypes.ts
+++ app/data-catalog/DataCatalogTypes.ts
@@ -1,84 +1,106 @@
-import { Feature, Polygon } from "geojson";
+import { SvgIconTypeMap } from "@mui/material"
+import { OverridableComponent } from "@mui/material/OverridableComponent"
+import { Feature, Polygon } from "geojson"
+
+export type CategorySummary = {
+	id: string
+	name: string
+	icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string }
+	description: string
+}
+
+export type Category = CategorySummary & {
+	resources: ResourceSummary[]
+}
 
 export type License = {
-  id: string,
-  name: string,
-  description: string,
-  url: string,
+	id: string
+	name: string
+	description: string
+	url: string
+}
+
+export type ProviderSummary = {
+	id: string
+	name: string
+	description: string
+	url: string
 }
 
 export type Provider = {
-  id: string,
-  name: string,
-  description: string,
-  url: string
+	id: string
+	name: string
+	description?: string
+	url: string
+	resources: ResourceSummary[]
 }
 
 export type UseExample = {
-  id: string,
-  title: string,
-  description: string,
-  example_url: string
+	id: string
+	title: string
+	description: string
+	example_url: string
 }
 
 export type GlobalSpatialExtent = {
-  id: string,
-  type: "Global",
-  details?: string,
-  spatialResolution?: string
+	id: string
+	type: "Global"
+	details?: string
+	spatialResolution?: string
 }
 
 export type RegionSpatialExtent = {
-  id: string,
-  type: "Region",
-  region: string,
-  details?: string,
-  geometry: Feature<Polygon>
-  spatialResolution?: string
+	id: string
+	type: "Region"
+	region: string
+	details?: string
+	geometry: Feature<Polygon>[]
+	spatialResolution?: string
 }
 
 export type SpatialExtent = GlobalSpatialExtent | RegionSpatialExtent
 
 export type Code = {
-  id: string,
-  language: "Java" | "Javascript" | "Python" | "Go" | "Bash"
-  code: string
+	id: string
+	language: "Java" | "Javascript" | "Python" | "Go" | "Bash"
+	code: string
 }
 
 export type CodeExample = {
-  id: string,
-  title: string,
-  description: string,
-  code: Code[]
+	id: string
+	title: string
+	description?: string
+	code: Code[]
 }
 
 export type ResourceSummary = {
-  id: string,
-  title: string,
-  abstract: string,
-  type: "API" | "Dataset" | "ML Model",
+	id: string
+	title: string
+	abstract: string
+	type: "API" | "Dataset" | "ML Model"
 }
 
 export type Resource = {
-  id: string,
-  title: string,
-  abstract: string,
-  type: "API" | "Dataset" | "ML Model",
-  html_content: string,
-  openapi_url?: string,
-  download_url?: string,
-  github_url?: string,
-  documentation_url?: string,
-  update_frequency: string,
-  release_date: string,
-  client_library: boolean,
-  keywords: string[],
-  version: string,
-  license: License,
-  basedOn?: ResourceSummary[],
-  usedBy?: ResourceSummary[],
-  providers: Provider[],
-  useExamples?: UseExample[],
-  spatialExtent?: SpatialExtent[],
-  codeExamples?: CodeExample[]
+	id: string
+	title: string
+	abstract: string
+	type: "API" | "Dataset" | "ML Model"
+	html_content?: string
+	openapi_url?: string
+	download_url?: string
+	github_url?: string
+	documentation_url?: string
+	update_frequency?: string
+	release_date?: string
+	client_library: boolean
+	keywords: string[]
+	version: string
+	license: License
+	basedOn?: ResourceSummary[]
+	usedBy?: ResourceSummary[]
+	providers: ProviderSummary[]
+	useExamples?: UseExample[]
+	spatialExtent?: SpatialExtent[]
+	codeExamples?: CodeExample[]
+	categories: CategorySummary[]
 }
