bab1ca2069eb4f6ccde6b96964323dfb761a85ee
Sander Krøglid <sander.kroglid@knowit.no>
2025-06-30 12:49:15 +0200

    fix: new front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import Box from "@mui/material/Box"
-import { CircleSpinner } from "@/components/CircleSpinner"
 import Typography from "@mui/material/Typography"
 import InfoCard from "@/components/InfoCard"
 import { JavascriptLogo } from "@/icons/JavascriptLogo"
@@ -22,40 +20,64 @@ const Home = () => {
 	const { data: session } = useSession()
 
 	return (
-		<Box className="lg:mb-28 mb-16 w-full">
-			<Box className="flex lg:flex-row flex-col">
-				<Box className="relative flex flex-col bg-primary-90 lg:w-1/2 overflow-hidden">
-					<CircleSpinner />
-					<Typography
-						variant="h1"
-						className="flex-1 py-48 text-4xl xs:text-6xl w-full h-full lg:w-[40rem] px-8 pr-16 lg:ml-auto z-10"
-					>
-						<b>Global open source data</b>
-						<br /> enabling local innovation
-					</Typography>
-				</Box>
-				<Box className="flex flex-col lg:w-1/2 gap-10 justify-center py-16 px-8 bg-neutral-90">
-					<Box className="flex flex-row gap-2">
-						<LockOpenOutlined className="text-4xl" />
-						<Typography className="text-2xl xs:text-3xl">
-							Truly open source
+		<div className="w-full">
+			<div className="flex flex-col bg-[url('/matt-benson-hero-unsplash.jpg')] bg-cover bg-center py-12 sm:py-48">
+				<div className="flex flex-col text-primary-99 w-full sm:max-w-7xl sm:mx-auto px-8 gap-12 pb-4 justify-center">
+					<div className="flex flex-col gap-2 xs:gap-3">
+						<Typography
+							variant="h1"
+							className="flex-1 text-4xl xs:text-6xl text-shadow-lg"
+						>
+							Global open source data
 						</Typography>
-					</Box>
-					<Box className="flex flex-row gap-2">
-						<VerifiedUserOutlined className="text-4xl" />
-						<Typography className="text-2xl xs:text-3xl">
-							Verified and quality assured data
+						<Typography
+							variant="h2"
+							className="flex-1 text-3xl xs:text-5xl text-shadow-lg"
+						>
+							enabling local innovation
 						</Typography>
-					</Box>
-					<Box className="flex flex-row gap-2">
-						<LightbulbOutlined className="text-4xl" />
-						<Typography className="text-2xl xs:text-3xl">
-							Built to support innovation
-						</Typography>
-					</Box>
-				</Box>
-			</Box>
-			<div className="py-20">
+					</div>
+					<div className="flex flex-col gap-4 justify-center">
+						<div className="flex flex-row gap-2 items-center">
+							<LockOpenOutlined className="text-2xl xs:text-4xl text-shadow-lg" />
+							<Typography className="text-xl xs:text-3xl text-shadow-lg">
+								Truly open source
+							</Typography>
+						</div>
+						<div className="flex flex-row gap-2">
+							<VerifiedUserOutlined className="text-2xl xs:text-4xl text-shadow-lg" />
+							<Typography className="text-xl xs:text-3xl text-shadow-lg">
+								Verified and quality assured data
+							</Typography>
+						</div>
+						<div className="flex flex-row gap-2">
+							<LightbulbOutlined className="text-2xl xs:text-4xl text-shadow-lg" />
+							<Typography className="text-xl xs:text-3xl text-shadow-lg">
+								Built to support innovation
+							</Typography>
+						</div>
+					</div>
+					<div className="grid md:grid-cols-2 gap-4 w-fit">
+						<Link href="/data-catalog">
+							<Button
+								variant="contained"
+								className="rounded-full normal-case shadow-none text-xl px-8 py-4 self-start w-full"
+							>
+								Explore our data catalog
+							</Button>
+						</Link>
+						<Link href="/resources">
+							<Button
+								variant="contained"
+								className="bg-primary-98 hover:bg-neutral-95 text-neutral-0 rounded-full normal-case shadow-none text-xl px-8 py-4 self-start w-full"
+							>
+								Explore our resources
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="bg-neutral-95 py-16">
 				<div className="flex flex-col gap-10 sm:w-full sm:max-w-7xl sm:mx-auto px-8">
 					<Typography className="text-3xl xs:text-4xl">
 						Open data Hackathon 2025
@@ -75,62 +97,7 @@ const Home = () => {
 					</Link>
 				</div>
 			</div>
-			{!session && (
-				<Box className="bg-neutral-95 py-20">
-					<Box className="flex flex-col-reverse lg:flex-row gap-14 sm:w-full sm:max-w-7xl sm:mx-auto px-8">
-						<Box className="flex flex-1 flex-col gap-10">
-							<Typography className="text-3xl xs:text-4xl">
-								Explore our catalog and start building your innovation
-							</Typography>
-							<Typography className="text-xl xs:text-2xl">
-								OpenEPI is an enabler for the worlds open-source data, linking
-								developers to verified, high-quality data to support innovation.
-								While our APIs are available without authentication, signing up
-								can offer additional benefits like increased rate limits for
-								greater flexibility.
-							</Typography>
-							<Button
-								variant="contained"
-								className="rounded-full normal-case shadow-none text-xl px-8 py-4 self-start"
-								onClick={() =>
-									signIn("keycloak", { callbackUrl: "/dashboard" })
-								}
-							>
-								Register for increased rate limits
-							</Button>
-						</Box>
-						<Image
-							width={256}
-							height={256}
-							alt="about"
-							src="about-logo.svg"
-							className="w-40 h-40 xs:w-48 xs:h-48 lg:w-64 lg:h-64"
-						/>
-					</Box>
-				</Box>
-			)}
-			<Box className="flex flex-col sm:w-full sm:max-w-7xl sm:mx-auto mt-28 px-8">
-				<Box className="flex flex-col gap-8 mb-6">
-					<Typography variant="h2" className="text-3xl xs:text-4xl">
-						Data catalog of open data
-					</Typography>
-					<Typography className="text-xl xs:text-2xl">
-						We provide a range of climate and geospatial data.
-						<br />
-						OpenEPI, together with our partners, provide APIs, Datasets, ML
-						Models and more
-					</Typography>
-					<Link href="/data-catalog" className="lg:w-fit w-full">
-						<Button
-							variant="contained"
-							className="rounded-full normal-case shadow-none text-xl px-8 py-4"
-						>
-							Explore all our data catalog
-						</Button>
-					</Link>
-				</Box>
-			</Box>
-			<div className="flex flex-col sm:w-full sm:max-w-7xl sm:mx-auto mt-28 px-8">
+			<div className="flex flex-col sm:w-full sm:max-w-7xl sm:mx-auto mt-16 px-8">
 				<div className="flex flex-col gap-8 mb-6">
 					<Typography variant="h2" className="text-3xl xs:text-4xl">
 						Categories
@@ -141,9 +108,9 @@ const Home = () => {
 				</div>
 				<Categories />
 			</div>
-			<Box className="flex flex-col sm:w-full sm:max-w-7xl sm:mx-auto mt-28 px-8">
-				<Box className="flex flex-col lg:mx-0 mx-2">
-					<Box className="flex flex-col gap-8 mb-16">
+			<div className="flex flex-col sm:w-full sm:max-w-7xl sm:mx-auto my-28 px-8">
+				<div className="flex flex-col lg:mx-0 mx-2">
+					<div className="flex flex-col gap-8 mb-16">
 						<Typography variant="h2" className="text-3xl xs:text-4xl">
 							Client libraries
 						</Typography>
@@ -151,8 +118,8 @@ const Home = () => {
 							To ease your integration with our APIs, we provide three client
 							libraries
 						</Typography>
-					</Box>
-					<Box className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+					</div>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 						<InfoCard
 							externalLink={true}
 							header="Javascript"
@@ -174,10 +141,54 @@ const Home = () => {
 							cardIcon={<JavaLogo />}
 							href="https://central.sonatype.com/artifact/io.openepi/openepi-client"
 						/>
-					</Box>
-				</Box>
-			</Box>
-		</Box>
+					</div>
+				</div>
+			</div>
+			<div className="bg-neutral-95 py-20">
+				<div className="flex flex-col lg:flex-row gap-14 sm:w-full sm:max-w-7xl sm:mx-auto px-8 items-center">
+					<div className="flex flex-1 flex-col gap-10 w-full">
+						<Typography className="text-3xl xs:text-4xl">
+							Explore our catalog and start building your innovation
+						</Typography>
+						<Typography className="text-xl xs:text-2xl">
+							OpenEPI is an enabler for the worlds open-source data, linking
+							developers to verified, high-quality data to support innovation.
+							We provide a range of climate and geospatial data. OpenEPI,
+							together with our partners, provide APIs, Datasets, ML Models and
+							more
+						</Typography>
+						<div className="grid md:grid-cols-2 gap-4 w-fit">
+							<Link href="/resources">
+								<Button
+									variant="contained"
+									className="rounded-full normal-case shadow-none text-xl px-8 py-4 self-start w-full"
+								>
+									Explore our data catalog
+								</Button>
+							</Link>
+							{!session && (
+								<Button
+									variant="outlined"
+									className="rounded-full normal-case shadow-none text-xl px-8 py-4 self-start w-full"
+									onClick={() =>
+										signIn("keycloak", { callbackUrl: "/dashboard" })
+									}
+								>
+									Register for increased rate limits
+								</Button>
+							)}
+						</div>
+					</div>
+					<Image
+						width={256}
+						height={256}
+						alt="about"
+						src="about-logo.svg"
+						className="w-40 h-40 xs:w-48 xs:h-48 lg:w-64 lg:h-64"
+					/>
+				</div>
+			</div>
+		</div>
 	)
 }
 
