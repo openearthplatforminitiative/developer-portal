09b51b04531d2a7a9b3706f936ece7a00d3a3f6b
Sander Krøglid <sander.kroglid@knowit.no>
2025-06-30 16:12:56 +0200

    format

--- src/app/how-tos/[slug]/page.tsx
+++ src/app/how-tos/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import PortableTextStyled from "@/components/PortableTextStyled/PortableTextStyled"
 import { BackIcon } from "@/icons/BackIcon"
-import {
-	fetchResourceTutorialBySlug,
-} from "@/sanity/api"
+import { fetchResourceTutorialBySlug } from "@/sanity/api"
 import { Tooltip, Typography } from "@mui/material"
 import Link from "next/link"
 import { notFound } from "next/navigation"
